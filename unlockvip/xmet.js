b67c1feeb415e2b76de5312f6c40e8055c1a9837
whitepaper2023 <122159843+whitepaper2023@users.noreply.github.com>
2023-06-26 21:09:57 +0800

    Update xmet.js

--- unlockvip/xmet.js
+++ unlockvip/xmet.js
@@ -3,22 +3,14 @@
 
 脚本名称: 喜马拉雅儿童
 脚本功能：解锁所有Vip课程以及音频直接看
-下载地址：appstore
-软件版本：所有
 脚本作者：伟人
-作者QQ:
-更新时间：2022-12-16
-问题反馈：QQ+55749353
-QQ会员群：添加作者
-TG反馈群：https://t.me/WeiRenOvO
-TG频道群：https://t.me/WeiRenQAQ
-使用声明：⚠️此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+
 
 
 [rewrite_local]
 
-^http[s]?:\/\/((.+ximalaya.+)|(.+xmcdn.+))(album\/v2\/trackRecord\/queryTrackRecordsByAlbumIdAndUid|user\/member\/page\/productdetails).*$ url script-response-body https://raw.githubusercontent.com/WeiRen0/Scripts/main/xmet.js
-^http[s]?:\/\/((.+ximalaya.+)|(.+xmcdn.+))(queryTrackRecordsByAlbumIdAndUid|album\/trackRecord\/querySampleTrack|album\/trackRecord\/palyTrack).*$ url script-request-header https://raw.githubusercontent.com/WeiRen0/Scripts/main/xmlyet.js
+^http[s]?:\/\/((.+ximalaya.+)|(.+xmcdn.+))(album\/v2\/trackRecord\/queryTrackRecordsByAlbumIdAndUid|user\/member\/page\/productdetails).*$ url script-response-body https://raw.githubusercontent.com/whitepaper2023/myquantumult/main/unlockvip/xmet.js
+^http[s]?:\/\/((.+ximalaya.+)|(.+xmcdn.+))(queryTrackRecordsByAlbumIdAndUid|album\/trackRecord\/querySampleTrack|album\/trackRecord\/palyTrack).*$ url script-request-header https://raw.githubusercontent.com/whitepaper2023/myquantumult/main/unlockvip/xmlyet.js
 
 hostname = *xima*,*.xmcdn.com,180.153.255.*,180.153.140.*,180.153.250.*,114.80.99.*,114.80.139.237,114.80.161.29,1.62.62.64,51*.com,180.153.*.*,*xima*,*xmcdn*,*.ximalaya.com,*.xmcdn.com,180.153.255.*,180.153.140.*,180.153.250.*,114.80.99.*,114.80.139.237,114.80.161.29,1.62.62.64,51*.com
 
