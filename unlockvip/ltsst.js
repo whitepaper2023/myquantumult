030ba39028fda828da86195b5fb9951f917695ee
whitepaper2023 <122159843+whitepaper2023@users.noreply.github.com>
2023-06-28 19:27:23 +0800

    Add files via upload

---
+++ unlockvip/ltsst.js
@@ -0,0 +1,40 @@
+/**********************************************
+脚本作者：是豆豆呀
+脚本功能：旅途随身听(APP和微信小程序) 解锁会员
+使用声明：⚠️仅供学习交流，🈲️商业用途
+
+
+[rewrite_local]
+^https:\/\/www\.1314zhilv\.com\/ltsstnew\/(userApplet|user)\/getInfo url script-response-body https://raw.githubusercontent.com/jizhi0520/QX/main/ltsst.js
+
+
+
+[MITM]
+hostname = www.1314zhilv.com
+**********************************************/
+
+
+let obj = JSON.parse($response.body);
+   
+    obj = {
+   "state": 200,
+   "message": "success",
+   "content": {
+     "isAnchor": 1,
+     "isShowAppletWX": 1,
+     "identityType": 1,
+     "isShowAppletDY": 1,
+     "levDesc": "SVIP",
+     "headUrl": "https://raw.githubusercontent.com/jizhi0520/QX/main/DouDou.JPG",
+     "isShowGrey": 1,
+     "phoneNum": "是豆豆呀",
+     "isMountSwitch": 0,
+     "userName": "是豆豆呀",
+     "userId": "是豆豆呀",
+     "isShowGrey" : 0,
+     "isActivate": 1
+   },
+   "success": true
+ }
+
+$done({body : JSON.stringify(obj)});
