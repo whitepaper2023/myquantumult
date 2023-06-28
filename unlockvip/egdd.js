23a60e2d7010f3dcac5dc7241ad2269d0ba59b1a
whitepaper2023 <122159843+whitepaper2023@users.noreply.github.com>
2023-06-28 18:23:10 +0800

    Update egdd.js

--- unlockvip/egdd.js
+++ unlockvip/egdd.js
@@ -1,5 +1,5 @@
 /*
 Unlocks by Cuttlefish 公众号：墨鱼手记
 */
-var body = $response.body.replace(/vip":0/g, 'vip":1').replace(/vip_endtime":"0/g, 'vip_endtime":"1800000000')
+var body = $response.body.replace(/vip":0/g, 'vip":1').replace(/vip_endtime":"0/g, 'vip_endtime":"2800000000')
 $done({ body });
