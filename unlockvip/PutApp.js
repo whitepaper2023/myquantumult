fb117f5321a5754d3f4125109fa0c0602c0901b6
whitepaper2023 <122159843+whitepaper2023@users.noreply.github.com>
2023-07-11 20:09:13 +0800

    Update PutApp.js

--- unlockvip/PutApp.js
+++ unlockvip/PutApp.js
@@ -3,13 +3,12 @@
 项目名称：PutApp-应用收集
 下载地址：https://t.cn/A6pFrrY7
 脚本作者：chxm1023
-电报频道：https://t.me/chxm1023
-使用声明：⚠️仅供参考，🈲转载与售卖！
+
 
 **************************************
 
 [rewrite_local]
-^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/PutApp.js
+^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/whitepaper2023/myquantumult/main/unlockvip/PutApp.js
 
 [mitm]
 hostname = buy.itunes.apple.com
