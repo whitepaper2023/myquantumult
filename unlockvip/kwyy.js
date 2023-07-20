d37f2a0b7aa0a38df6812ac5d40a70a0c7798bf9
whitepaper2023 <122159843+whitepaper2023@users.noreply.github.com>
2023-07-20 19:14:04 +0800

    Update kwyy.js

--- unlockvip/kwyy.js
+++ unlockvip/kwyy.js
@@ -1,6 +1,6 @@
 /* 
 脚本功能：完美解锁酷我音乐
-脚本作者：king
+脚本作者：king（nameking77/Qx/main/rewrite/kw.js）
 下载地址：https://is.gd/CWqKMX
 更新时间：2022.8.16
 脚本频道：https://t.me/yqc_123
@@ -10,7 +10,7 @@
 当前版本:10.2.8
 [rewrite_local]
 # 完美解锁酷我音乐 听书+下载+解锁数字专辑
-^https?:\/\/.*\.(kuwo|lrts)\.(cn|me)\/(a\.p|music\.pay|(vip\/(v2|enc)\/(theme|user\/vip))|(EcomResource|(Mobile)?Ad)Serv(er|ice)).* url script-response-body https://raw.githubusercontent.com/nameking77/Qx/main/rewrite/kw.js
+^https?:\/\/.*\.(kuwo|lrts)\.(cn|me)\/(a\.p|music\.pay|(vip\/(v2|enc)\/(theme|user\/vip))|(EcomResource|(Mobile)?Ad)Serv(er|ice)).* url script-response-body https://raw.githubusercontent.com/whitepaper2023/myquantumult/main/unlockvip/kwyy.js
 [mitm] 
 hostname = *.kuwo.cn, *.lrts.me
 ********************************
