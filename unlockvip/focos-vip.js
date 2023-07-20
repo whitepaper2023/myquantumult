bb355bf9b19684158e6b4384d43a15cd68975e4e
whitepaper2023 <122159843+whitepaper2023@users.noreply.github.com>
2023-07-20 19:19:28 +0800

    Update and rename focos.vip.js to focos-vip.js

--- unlockvip/focos-vip.js
+++ unlockvip/focos-vip.js
@@ -9,8 +9,8 @@
           
 [rewrite_local]
   
-# > focos☆解锁会员权限（2023-05-05）@ddgksf2013
-^https?:\/\/focos\.oracle\.bendingspoonsapps\.com\/v\d\/(users\/setup|purchases\/verify\/apple) url script-response-body focos.vip.js 
+# > focos解锁会员权限
+^https?:\/\/focos\.oracle\.bendingspoonsapps\.com\/v\d\/(users\/setup|purchases\/verify\/apple) url script-response-body https://raw.githubusercontent.com/whitepaper2023/myquantumult/main/unlockvip/focos-vip.js 
 
 
 [mitm]
