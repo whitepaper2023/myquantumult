f80040eb5fe9a3b683cbcb77a5f5d323dd0e112a
whitepaper2023 <122159843+whitepaper2023@users.noreply.github.com>
2023-07-20 20:56:20 +0800

    Update HashPhotos.js

--- unlockvip/HashPhotos.js
+++ unlockvip/HashPhotos.js
@@ -8,7 +8,7 @@
 **************************************
 
 [rewrite_local]
-^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/iTunes/HashPhotos.js
+^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/whitepaper2023/myquantumult/main/unlockvip/HashPhotos.js
 
 [mitm]
 hostname = buy.itunes.apple.com
