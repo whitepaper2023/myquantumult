bb6e2302e4b05338c6a89f90e71cc14afa268597
whitepaper2023 <122159843+whitepaper2023@users.noreply.github.com>
2023-07-20 19:23:24 +0800

    Add files via upload

---
+++ unmodified/focos-vip.js
@@ -0,0 +1,42 @@
+/***********************************
+
+> 应用名称：focos
+> 下载地址：http://t.cn/AilyJ3mp
+> 脚本作者：ddgksf2013
+> 解锁说明：解锁高级会员权限
+> 更新时间：2023-05-05
+
+          
+[rewrite_local]
+  
+# > focos☆解锁会员权限（2023-05-05）@ddgksf2013
+^https?:\/\/focos\.oracle\.bendingspoonsapps\.com\/v\d\/(users\/setup|purchases\/verify\/apple) url script-response-body focos.vip.js 
+
+
+[mitm]
+ 
+hostname = focos.oracle.bendingspoonsapps.com
+
+
+***********************************/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var obj=JSON.parse($response.body);obj.me.active_subscriptions_ids=["com.focos.1w_t8_1w"],$done({body:JSON.stringify(obj)});
