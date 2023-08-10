6c1ace9654868e26601fbc0b49d9a28bd30211a4
whitepaper <122159843+whitepaper2023@users.noreply.github.com>
2023-08-10 17:50:17 +0800

    Add files via upload

---
+++ unmodified/nicegram.js
@@ -0,0 +1,22 @@
+/*************************************
+
+项目名称：Nicegram
+下载地址：https://t.cn/A6ou0MCe
+脚本作者：chxm1023
+
+
+**************************************
+
+[rewrite_local]
+^https?:\/\/restore-access\.indream\.app\/restoreAccess url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/nicegram.js
+
+[mitm]
+hostname = restore-access.indream.app
+
+*************************************/
+
+
+const isQX = typeof $task != "undefined";
+const chxm1023 = {"data":{"premiumAccess": true}};
+console.log('已操作成功🎉\n叮当猫の分享频道: https://t.me/chxm1023');
+$done({status: isQX ? "HTTP/1.1 200 OK" : 200, headers: $response.headers, body: JSON.stringify(chxm1023)});
