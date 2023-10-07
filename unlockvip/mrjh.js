98275e3593b2e71be946090a8b0fd0277f6166e7
whitepaper <122159843+whitepaper2023@users.noreply.github.com>
2023-10-07 18:56:56 +0800

    Update mrjh.js

--- unlockvip/mrjh.js
+++ unlockvip/mrjh.js
@@ -8,8 +8,8 @@
 **************************************
 
 [rewrite_local]
-^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/Revenuecat/mrjh.js
-^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/chxm1023/Rewrite/main/Revenuecat/mrjh.js
+^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/whitepaper2023/myquantumult/main/unlockvip/mrjh.js
+^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/whitepaper2023/myquantumult/main/unlockvip/mrjh.js
 
 [mitm]
 hostname = api.revenuecat.com
