aca74922c9127eb73bda49b44517f83bafe328ad
whitepaper <122159843+whitepaper2023@users.noreply.github.com>
2023-10-07 18:53:56 +0800

    Add files via upload

---
+++ unmodified/mrjh.js
@@ -0,0 +1,47 @@
+/*************************************
+
+项目名称：Structured-每日计划
+下载地址：https://t.cn/A6cWhz4X
+脚本作者：chxm1023
+
+
+**************************************
+
+[rewrite_local]
+^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/Revenuecat/mrjh.js
+^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/chxm1023/Rewrite/main/Revenuecat/mrjh.js
+
+[mitm]
+hostname = api.revenuecat.com
+
+*************************************/
+
+
+const chxm1023 = {};
+const chxm1024 = JSON.parse(typeof $response != "undefined" && $response.body || null);
+
+const name = "pro";
+const appid = "today.structured.pro";
+
+  
+if (typeof $response == "undefined") {
+  delete $request.headers["x-revenuecat-etag"];
+  delete $request.headers["X-RevenueCat-ETag"];
+  chxm1023.headers = $request.headers;
+} else if (chxm1024 && chxm1024.subscriber) {
+  data = {
+ "Author": "chxm1023",
+ "Telegram" : "https://t.me/chxm1023",
+ "warning": "仅供学习，禁止转载或售卖",
+ "original_purchase_date": "2022-09-09T09:09:09Z",
+ "purchase_date": "2022-09-09T09:09:09Z",
+ "store" : "app_store",
+ "ownership_type": "PURCHASED"
+ };
+  chxm1024.subscriber.subscriptions[(appid)] = data
+  chxm1024.subscriber.entitlements[(name)] = JSON.parse(JSON.stringify(data));
+  chxm1024.subscriber.entitlements[(name)].product_identifier = (appid);
+  chxm1023.body = JSON.stringify(chxm1024);
+}
+
+$done(chxm1023);
